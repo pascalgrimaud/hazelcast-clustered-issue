40fb4ef7c5b568dcf291929e0d0d12bf0805cd5c
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-11-28 08:06:36 +0100

    Generate project from master 8680dc3eeb3437fc4dc851f563d132777029a23b

---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('jhazsessApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
---
+++ src/main/webapp/app/services/auth/password.service.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('jhazsessApp')
+        .factory('Password', Password);
+
+    Password.$inject = ['$resource'];
+
+    function Password($resource) {
+        var service = $resource('api/account/change_password', {}, {});
+
+        return service;
+    }
+})();
